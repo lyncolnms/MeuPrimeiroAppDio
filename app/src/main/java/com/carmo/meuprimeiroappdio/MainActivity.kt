8f94270bfa9cfdec25878d4f672df356cb5b62c4
Lyncoln Mauricio <lyncolnms@gmail.com>
2025-11-03 14:00:53 -0300

    Initial commit

---
+++ app/src/main/java/com/carmo/meuprimeiroappdio/MainActivity.kt
@@ -0,0 +1,39 @@
+package com.carmo.meuprimeiroappdio
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import com.carmo.meuprimeiroappdio.ui.theme.MeuPrimeiroAppDioTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+
+        setContent {
+            MeuPrimeiroAppDioTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = getString(R.string.greeting),
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = name,
+        modifier = modifier
+    )
+}
